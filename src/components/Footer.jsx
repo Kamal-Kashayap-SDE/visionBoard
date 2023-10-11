b6d7d8a1ebdaa9299ac0d5a069c9d6cd37edf3c2
Kamal-Kashayap-SDE <115485467+Kamal-Kashayap-SDE@users.noreply.github.com>
2023-10-11 10:27:13 +0530

    initialcommit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2023 All rights of <b>@VisionBoard</b> are reserved by <b>Kamal Kashyap</b> 
+    </p>
+  </div>
+);
+
+export default Footer;
